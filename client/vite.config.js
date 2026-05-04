658fe7cb5757c63fb9fef67a239832537eb65c23
Bongdepchaii <buitrongthanh2k5@gmail.com>
2026-05-05 00:19:45 +0700

    feat: optimization folder React and design theme home SPA

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,         // cho phép truy cập từ bên ngoài Docker container
+    port: 5173,         // cổng host (đã match với docker-compose.yml)
+    watch: {
+      usePolling: true, // ✅ quan trọng khi chạy Docker
+      interval: 500,      // polling mỗi 500ms (tăng giảm tùy ý)
+      binaryInterval: 500
+    }
+  }
 })
